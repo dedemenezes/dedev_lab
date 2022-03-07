60ae55578d7efc5d31745c0f899cf9c0bbb15a20
Andre Menezes <djdedemenezes@gmail.com>
2022-03-07 13:15:38 -0300

    installed flatpickr package, generated controller and imported flatipickr css

---
+++ app/javascript/controllers/flatpicker_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from 'stimulus'
+import flatpickr from "flatpickr";
+
+export default class extends Controller {
+  connect() {
+    console.log("Hello from flatpickr controller zo/")
+  }
+}
