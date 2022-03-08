32ea9bc6198e860668c37fb8b76401200df2e982
Andre Menezes <djdedemenezes@gmail.com>
2022-03-08 08:39:45 -0300

    removd pry

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,8 @@
-require 'pry-byebug'
 class MoviesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
     if params[:query].present?
       @movies = PgSearch.multisearch(params[:query])
-      # binding.pry
       respond_to do |format|
         format.html # Follow regular flow of Rails
         format.text { render partial: 'movies_and_tv_shows.html', locals: { movies: @movies } }
@@ -16,7 +14,5 @@ class MoviesController < ApplicationController
         format.text { render partial: 'movie.html', locals: { movies: @movies } }
       end
     end
-    # binding.pry
-          # format.text { render partial: 'list.html', locals: { movies: @movies } }
   end
 end
