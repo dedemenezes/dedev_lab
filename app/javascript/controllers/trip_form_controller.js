5d478327828f46169e7fe31626257af7a45f488c
Andre Menezes <djdedemenezes@gmail.com>
2022-03-14 15:46:56 -0300

    hide next button when reaching submit input

--- app/javascript/controllers/trip_form_controller.js
+++ app/javascript/controllers/trip_form_controller.js
@@ -1,16 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["input"]
+  static targets = ["input", "next"]
 
   connect() {
   }
 
   next(event) {
     this.active = this.inputTargets.filter(target => target.classList.contains('show_input'))[0]
-    this.active.classList.remove('show_input')
-    this.active.classList.add('hide_input')
-    this.active.nextElementSibling.classList.remove('hide_input')
-    this.active.nextElementSibling.classList.add('show_input')
+    if (this.active.nextElementSibling.nextElementSibling === null) {
+      this.fowardSlideShow(this.active)
+      this.nextTarget.classList.add('hide_input')
+    } else {
+      this.fowardSlideShow(this.active)
+    }
+  }
+
+  fowardSlideShow(active) {
+    active.classList.remove('show_input')
+    active.classList.add('hide_input')
+    active.nextElementSibling.classList.remove('hide_input')
+    active.nextElementSibling.classList.add('show_input')
   }
 }
