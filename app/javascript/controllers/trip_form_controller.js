b59481ba26f1f4cf337b0f37c91b700e7219db40
Andre Menezes <djdedemenezes@gmail.com>
2022-03-14 15:18:25 -0300

    refactor using same input as target and also hiding submit button and diplaying only after all inputs

--- app/javascript/controllers/trip_form_controller.js
+++ app/javascript/controllers/trip_form_controller.js
@@ -1,22 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["title", "synopsis", "original_title" ]
+  static targets = ["input"]
 
   connect() {
   }
 
   next(event) {
-    if (this.titleTarget.classList.contains("show_input")) {
-      this.titleTarget.classList.remove('show_input')
-      this.titleTarget.classList.add('hide_input')
-      this.synopsisTarget.classList.remove('hide_input')
-      this.synopsisTarget.classList.add('show_input')
-    } else if (this.synopsisTarget.classList.contains("show_input")) {
-      this.synopsisTarget.classList.remove('show_input')
-      this.synopsisTarget.classList.add('hide_input')
-      this.original_titleTarget.classList.remove('hide_input')
-      this.original_titleTarget.classList.add('show_input')
-    }
+    this.active = this.inputTargets.filter(target => target.classList.contains('show_input'))[0]
+    this.active.classList.remove('show_input')
+    this.active.classList.add('hide_input')
+    this.active.nextElementSibling.classList.remove('hide_input')
+    this.active.nextElementSibling.classList.add('show_input')
   }
 }
