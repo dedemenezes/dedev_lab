4130177f4b1df21111d16bdccb9d8e8e71d0962b
Andre Menezes <djdedemenezes@gmail.com>
2022-06-15 22:59:28 -0300

    removed pry

--- app/models/concerns/capacity_validator.rb
+++ app/models/concerns/capacity_validator.rb
@@ -1,6 +1,5 @@
 class CapacityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    # binding.pry
     unless value.assignments.size < value.capacity
       record.errors.add attribute, (options[:message] || "team is full")
     end
