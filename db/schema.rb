1d6cda764c0268f558496f4421de6f71630ac6a3
Andre Menezes <djdedemenezes@gmail.com>
2022-06-19 18:31:59 -0300

    why

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_19_044744) do
+ActiveRecord::Schema.define(version: 2022_06_19_211807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 2022_06_19_044744) do
     t.string "poster_url"
     t.string "backdrop_url"
     t.date "release_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
